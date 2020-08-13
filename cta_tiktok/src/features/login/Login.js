f195ba75272348b6a0afbc35e3191cf08cf5d407
Ashya <ashyamanning@pursuit.org>
2020-08-13 01:51:16 -0400

    setting up login with email

--- cta_tiktok/src/features/login/Login.js
+++ cta_tiktok/src/features/login/Login.js
@@ -1,7 +1,29 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login } from "../../util/firebaseFunctions";
+import { updateUser } from "../../token/authSlice";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await login(email, password);
+            dispatch(updateUser(res.user));
+            history.push("/trending");
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
+
     return (
         <>
             <div>
@@ -10,10 +32,16 @@ const Login = () => {
             <div>
                 <p>Manage your account, check notifications, comment on videos, and more.</p>
             </div>
-            
+            <div className="loginFormContainer">
+                {error ? <div>{error}</div> : null}
+                <form onSubmit={handleSubmit} className="loginForm">
+                    <input type="email" name="Email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" name="Password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <button type="submit">Log in with Email</button>
+                </form>
+            </div>
             <div>
-                {/* <button type="submit" onClick={}>Use phone / email / username</button>
-                <button type="submit" onClick={}>Log in with Facebook</button>
+                {/* <button type="submit" onClick={}>Log in with Facebook</button>
                 <button type="submit" onClick={}>Log in with Google</button>
                 <button type="submit" onClick={}>Log in with Twitter</button>
                 <button type="submit" onClick={}>Log in with Apple</button>
