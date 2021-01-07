7828a9120f8b284fd4478831a26e2e996256b18d
Ashya <ashyamanning@pursuit.org>
2021-01-07 18:24:57 -0500

    fixing schema id constraint

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -48,27 +48,27 @@ CREATE TABLE hashtags (
     body TEXT
 );
 
-INSERT INTO users (id, firstName, lastName, userName, bio, profile_pic_url) 
+INSERT INTO users (firstName, lastName, userName, bio, profile_pic_url) 
 VALUES 
-    ('1', 'Tessa', 'Barrett', 't_time', 'Tessa in the house!', 'https://images.photowall.com/products/44013/daisy-close-up.jpg?h=699&q=85'),
-    ('2', 'Rocky', 'Queen', 'rocket', 'Off this!', 'https://images.pexels.com/photos/1547248/pexels-photo-1547248.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
-    ('3', 'Jade', 'Moon', 'Jbabyyy', 'I am eternal.', 'https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
-    ('4', 'Lamina', 'Williams', 'memo67', 'I just want to be happy.', 'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
-    ('5', 'Maria', 'Martinez', 'marializa', 'Im a bad ass baker!', 'https://images.pexels.com/photos/3115708/pexels-photo-3115708.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
-    ('6', 'Marvin', 'Thompson', 'earrocker', 'I breathe music!', 'https://images.pexels.com/photos/2240766/pexels-photo-2240766.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
-    ('7', 'Henry', 'Nunez', 'theflashwishes', 'Try and outrun me if you can.', 'https://images.pexels.com/photos/4006567/pexels-photo-4006567.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500');
+    ('Tessa', 'Barrett', 't_time', 'Tessa in the house!', 'https://images.photowall.com/products/44013/daisy-close-up.jpg?h=699&q=85'),
+    ('Rocky', 'Queen', 'rocket', 'Off this!', 'https://images.pexels.com/photos/1547248/pexels-photo-1547248.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
+    ('Jade', 'Moon', 'Jbabyyy', 'I am eternal.', 'https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
+    ('Lamina', 'Williams', 'memo67', 'I just want to be happy.', 'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
+    ('Maria', 'Martinez', 'marializa', 'Im a bad ass baker!', 'https://images.pexels.com/photos/3115708/pexels-photo-3115708.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
+    ('Marvin', 'Thompson', 'earrocker', 'I breathe music!', 'https://images.pexels.com/photos/2240766/pexels-photo-2240766.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
+    ('Henry', 'Nunez', 'theflashwishes', 'Try and outrun me if you can.', 'https://images.pexels.com/photos/4006567/pexels-photo-4006567.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500');
 
-INSERT INTO posts (id, poster_id, video_url, caption)
+INSERT INTO posts (poster_id, video_url, caption)
 VALUES
-    ('1', '1', 'https://www.joe.ie/life-style/9-of-the-best-60-seconds-or-less-youtube-videos-551969', 'HAHA'),
-    ('2', '2', 'https://www.youtube.com/watch?v=i5DjkBBonlo', 'This is weird'),
-    ('3', '3', 'https://www.youtube.com/watch?v=hcKtMjrZ0Dg', 'Ummmm...'),
-    ('4', '4', 'https://www.buzzfeed.com/lyapalater/youtube-videos-under-60-seconds-long-that-you-need-to', 'Whaaaaat?!'),
-    ('5', '5', 'https://www.youtube.com/watch?v=H1gHwfFGtcg', 'What just happened?'),
-    ('6', '6', 'https://www.youtube.com/watch?v=Y8KiYqg077w', 'That was cool'),
-    ('7', '7', 'https://www.youtube.com/watch?v=eQqebq_gfzM', 'Awwww'),
-    ('8', '4', 'https://www.pinterest.com/pin/309692911869563856/', 'Take it or leave it.'),
-    ('9', '6', 'https://www.youtube.com/watch?v=sjG_XLaOXf4', 'I does this!!'),
-    ('10', '5', 'https://www.youtube.com/watch?v=kY3qxdTBXOU', 'Try it!'),
-    ('11', '7', 'https://www.youtube.com/watch?v=p2vEoNuRvrA', 'Really?!');
+    ('1', 'https://www.joe.ie/life-style/9-of-the-best-60-seconds-or-less-youtube-videos-551969', 'HAHA'),
+    ('2', 'https://www.youtube.com/watch?v=i5DjkBBonlo', 'This is weird'),
+    ('3', 'https://www.youtube.com/watch?v=hcKtMjrZ0Dg', 'Ummmm...'),
+    ('4', 'https://www.buzzfeed.com/lyapalater/youtube-videos-under-60-seconds-long-that-you-need-to', 'Whaaaaat?!'),
+    ('5', 'https://www.youtube.com/watch?v=H1gHwfFGtcg', 'What just happened?'),
+    ('6', 'https://www.youtube.com/watch?v=Y8KiYqg077w', 'That was cool'),
+    ('7', 'https://www.youtube.com/watch?v=eQqebq_gfzM', 'Awwww'),
+    ('4', 'https://www.pinterest.com/pin/309692911869563856/', 'Take it or leave it.'),
+    ('6', 'https://www.youtube.com/watch?v=sjG_XLaOXf4', 'I does this!!'),
+    ('5', 'https://www.youtube.com/watch?v=kY3qxdTBXOU', 'Try it!'),
+    ('7', 'https://www.youtube.com/watch?v=p2vEoNuRvrA', 'Really?!');
     
